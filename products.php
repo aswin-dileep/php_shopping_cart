1ce42a262f69d9c699f8e988258db0dc56571b17
Aswin <aswindilip5@gmail.com>
2022-12-14 12:27:15 +0530

    product filter added

--- products.php
+++ products.php
@@ -1,3 +1,32 @@
+<?php
+include("./server/connect.php");
+
+
+  if(isset($_POST['filter_search']))  {
+
+       $price = $_POST['price'];
+
+       if(isset($_POST['Category'])){
+        $category =$_POST['Category'];
+        
+        $all_products_qry = "SELECT * FROM products WHERE product_category='$category' AND product_price<='$price'";
+        $all_products_result = mysqli_query($con, $all_products_qry);
+       }else{
+        $all_products_qry = "SELECT * FROM products WHERE  product_price<='$price'";
+     
+        $all_products_result = mysqli_query($con, $all_products_qry);
+       }
+  
+  }else{
+    $all_products_qry = "SELECT * FROM products";
+    $all_products_result = mysqli_query($con, $all_products_qry);
+    
+  }
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,9 +38,7 @@
     <!-- bootstrap link -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <!-- font awesome link -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./assets/css/styles.css">
 
 </head>
@@ -31,31 +58,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="products.html">Products</a>
                 </li>
-                <li class="nav-item">
-                    <div class="dropdown">
-                        <button class="btn  dropdown-toggle" type="button" id="dropdownMenuButton1"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Category
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item" href="#">Travel</a></li>
-                            <li><a class="dropdown-item" href="#">Casual</a></li>
-                            <li><a class="dropdown-item" href="#">Laptop</a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <div class="dropdown">
-                        <button class="btn dropdown-toggle" data-bs-toggle="dropdown">
-                            Brands
-                            <ul class="dropdown-menu">
-                                <li><a href="" class="dropdown-item">Nike</a></li>
-                                <li><a href="" class="dropdown-item">Puma</a></li>
-                                <li><a href="" class="dropdown-item">Addidas</a></li>
-                            </ul>
-                        </button>
-                    </div>
-                </li>
+
                 <li class="nav-item">
                     <a class="nav-link" href="contact.html">Contact Us</a>
                 </li>
@@ -78,66 +81,69 @@
     <div class=" mx-5 mt-5 ">
 
         <div class="row">
-            <div class='col-md-3 '>
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="../E-com/images/American Tourister 32 Ltrs.jpg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text text-center">500/-</p>
-                        <a href="#" class="btn btn-primary">Add to Cart</a>
-                        <a href="./single-product.php " class="btn btn-secondary">View more</a>
-                    </div>
+        <h3 class="text-center mb-4">Our Products</h3>
+            <div class="col-md-2 " >
+                <h4 class="text-center">Search products</h4>
+
+                <h5 class=" mt-5">Category</h5>
+                 <form action="./products.php" method="POST">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="Category" value="Casual bags" >
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        Causal Bags
+                    </label>
                 </div>
-            </div>
-            
-            <div class='col-md-3  '>
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="../E-com/images/American Tourister 32 Ltrs.jpg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="Category" value="Travel bags" >
+                    <label class="form-check-label" for="flexRadioDefault2">
+                        Travel bags
+                    </label>
                 </div>
-            </div>
-            <div class='col-md-3  '>
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="../E-com/images/American Tourister 32 Ltrs.jpg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="Category" value="Handbags " >
+                    <label class="form-check-label" for="flexRadioDefault2">
+                        Handbags
+                    </label>
                 </div>
-            </div>
-            <div class='col-md-3  '>
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="../E-com/images/American Tourister 32 Ltrs.jpg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
+
+                <h5 class=" mt-5">Price</h5>
+                <input type="range" class="form-range " name="price" value="1000"  min="100" max="10000" id="customRange1">
+                <div class="">
+                    <span style="float: left;">100</span>
+                    <span style="float: right;">10000</span>
                 </div>
+                <div class="w-50 ">
+                <input type="submit" value="search" name="filter_search" class="form-control mt-3 btn btn-primary">
+
+                </div>
+                </form>
             </div>
-            <div class='col-md-3  '>
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="../E-com/images/American Tourister 32 Ltrs.jpg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
+            <div class="col-md-10">
+                <div class="row"> 
+                      
+                    <?php while ($all_products = mysqli_fetch_array($all_products_result)) { ?>
+                        <div class='col-md-3 mb-3'>
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="../E-com/images/<?php echo $all_products['product_image1']; ?>" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php echo $all_products['product_name'] ?></h5>
+                                    <p class="card-text text-center"><?php echo $all_products['product_price']; ?></p>
+                                    <a href="./single-product.php?product_id=<?php echo $all_products['product_id'] ?>" class="btn btn-primary">Buy Now</a>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    <?php } ?>
+
                 </div>
             </div>
         </div>
+
+
     </div>
-  
-    <footer class="mb-0">
+
+    <footer class="mb-0 mt-5">
         <div class=" bg-primary p-3 text-light ">
             <p class="text-center ">@copyright 2022</p>
         </div>
@@ -146,9 +152,7 @@
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>
